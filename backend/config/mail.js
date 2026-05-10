88bffa32cf6f4436c984af74bd92364bf827ce84
Tyagi72326 <anshtyagi1118@gmail.com>
2026-05-10 20:20:13 +0530

    updated mail.js file

--- backend/config/mail.js
+++ backend/config/mail.js
@@ -1,24 +1,31 @@
 import nodemailer from "nodemailer";
 
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
+
 export const sendOtpEmail = async (email, otp) => {
   try {
-    const transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-      },
-    });
+
+    await transporter.verify();
+    console.log("SMTP Connected");
 
     const info = await transporter.sendMail({
       from: `"MERN Auth" <${process.env.EMAIL_USER}>`,
       to: email,
       subject: "Your OTP Code",
       text: `Your OTP is: ${otp}`,
-      html: `<h2>Thanks for joining! Your verification code is ${otp}</h2>`
+      html: `<h2>Your verification code is ${otp}</h2>`,
     });
 
     console.log("EMAIL SENT:", info.response);
+
   } catch (error) {
     console.log("EMAIL ERROR:", error);
   }
